93163fa65c65e4ac459ed51ce0b52b8e908215cc
Mishra Ji <102230565+MishraJi-Devloper@users.noreply.github.com>
2023-12-24 23:38:28 +0530

    Update Student_Management_System.c

--- Student_Management_System.c
+++ Student_Management_System.c
@@ -5,90 +5,90 @@
 
 struct Chatrya {
     char id[20];
-    char name[50];
+    char naam[50];
     float gpa;
 };
 
-struct Chatrya students[SABSE_JYADAcHATRYA];
-int Chatraya_Sankhya = 0;
+struct Chatrya chhatra[SABSE_JYADAcHATRYA];
+int chhatraSankhya = 0;
 
-void addStudent() {
-    if ( Chatraya_Sankhya< SABSE_JYADAcHATRYA) {
-        printf("Enter student ID: ");
-        scanf("%s", students[Chatraya_Sankhya].id);
+void chhatraJod() {
+    if (chhatraSankhya < SABSE_JYADAcHATRYA) {
+        printf("Chatra ID dalein: ");
+        scanf("%s", chhatra[chhatraSankhya].id);
 
-        printf("Enter student name: ");
-        scanf(" %[^\n]", students[Chatraya_Sankhya].name);
+        printf("Chatra ka naam dalein: ");
+        scanf(" %[^\n]", chhatra[chhatraSankhya].naam);
 
-        printf("Enter student GPA: ");
-        scanf("%f", &students[Chatraya_Sankhya].gpa);
+        printf("Chatra ka GPA dalein: ");
+        scanf("%f", &chhatra[chhatraSankhya].gpa);
 
-      Chatraya_Sankhya++;
-        printf("\nStudent added successfully!\n");
+        chhatraSankhya++;
+        printf("\nChatra saflta purvak joda gaya!\n");
     } else {
-        printf("Maximum number of students reached.\n");
+        printf("Adhik se adhik chatra sankhya tak pahunch gaya hai.\n");
     }
 }
 
-void displayStudents() {
-    printf("\nStudent List:\n");
-    printf("%-15s %-30s %-10s\n", "ID", "Name", "GPA");
+void chhatraPradarshit() {
+    printf("\nChatra Suchi:\n");
+    printf("%-15s %-30s %-10s\n", "ID", "Naam", "GPA");
 
-    for (int i = 0; i < Chatraya_Sankhya; i++) {
-        printf("%-15s %-30s %-10.2f\n", students[i].id, students[i].name, students[i].gpa);
+    for (int i = 0; i < chhatraSankhya; i++) {
+        printf("%-15s %-30s %-10.2f\n", chhatra[i].id, chhatra[i].naam, chhatra[i].gpa);
     }
 }
 
-void searchStudent() {
-    char searchId[20];
-    printf("Enter student ID to search: ");
-    scanf("%s", searchId);
+void chhatraKhoj() {
+    char khojID[20];
+    printf("Khojne ke liye chatra ID dalein: ");
+    scanf("%s", khojID);
 
-    int found = 0;
-    for (int i = 0; i < Chatraya_Sankhya; i++) {
-        if (strcmp(students[i].id, searchId) == 0) {
-            printf("\nStudent found:\n");
-            printf("%-15s %-30s %-10s\n", "ID", "Name", "GPA");
-            printf("%-15s %-30s %-10.2f\n", students[i].id, students[i].name, students[i].gpa);
-            found = 1;
+    int mila = 0;
+    for (int i = 0; i < chhatraSankhya; i++) {
+        if (strcmp(chhatra[i].id, khojID) == 0) {
+            printf("\nChatra mil gaya:\n");
+            printf("%-15s %-30s %-10s\n", "ID", "Naam", "GPA");
+            printf("%-15s %-30s %-10.2f\n", chhatra[i].id, chhatra[i].naam, chhatra[i].gpa);
+            mila = 1;
             break;
         }
     }
 
-    if (!found) {
-        printf("\nStudent not found.\n");
+    if (!mila) {
+        printf("\nChatra nahi mila.\n");
     }
 }
 
 int main() {
-    int choice;
-        printf("<======| Welcome to Student Management System |======>\n");
+    int chunav;
+    printf("<======| Swagat hai Student Prabandhan Pranali mein |======>\n");
     do {
-        printf("\nStudent Database Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+        printf("\nChatra Database Prabandhan Pranali\n");
+        printf("1. Chatra Jod\n");
+        printf("2. Chatra Pradarshit\n");
+        printf("3. Chatra Khoj\n");
+        printf("4. Bahar Nikalein\n");
+        printf("Apna chunav dalein: ");
+        scanf("%d", &chunav);
 
-        switch (choice) {
+        switch (chunav) {
             case 1:
-                addStudent();
+                chhatraJod();
                 break;
             case 2:
-                displayStudents();
+                chhatraPradarshit();
                 break;
             case 3:
-                searchStudent();
+                chhatraKhoj();
                 break;
             case 4:
-                printf("\nExiting program. Goodbye!\n<======| Thank you for using Studnet Management System |======>\n");
+                printf("\nPrrogram se bahar nikal rahe hain. Alvida!\n<======| Student Prabandhan Pranali ka istemal karne ke liye dhanyavad |======>\n");
                 break;
             default:
-                printf("\nInvalid choice. Please try again.\n");
+                printf("\nAnvalid chunav. Kripaya phir se koshish karein.\n");
         }
-    } while (choice != 4);
+    } while (chunav != 4);
 
     return 0;
 }
